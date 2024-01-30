0e0a5cae7757483d2abe694359d3c2f2bffd988f
Michal Kukla <michal.kukla91@gmail.com>
2024-01-30 22:36:50 +0100

    router removed

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import BodyComponent from './components/BodyComponent.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <HeaderComponent />
     </div>
   </header>
-
-  <RouterView />
+  <body>
+    <BodyComponent />
+  </body>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+  margin: 0 auto;
+  justify-content: center;
 }
 
 .logo {
@@ -59,8 +55,6 @@ nav a:first-of-type {
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
@@ -69,15 +63,13 @@ nav a:first-of-type {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
+    justify-content: center;
     flex-wrap: wrap;
   }
 
   nav {
-    text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
-
     padding: 1rem 0;
     margin-top: 1rem;
   }
